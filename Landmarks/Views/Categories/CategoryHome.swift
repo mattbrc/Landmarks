e4e9aed07c8f23dbd626fa085c0f8e97dbeb1260
Matt <director@acidgambit.com>
2023-01-28 15:31:45 -0600

    add profile

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
+    
     var body: some View {
         NavigationView {
             List {
@@ -24,7 +26,19 @@ struct CategoryHome: View {
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
     }
 }
@@ -36,7 +50,14 @@ struct CategoryHome_Previews: PreviewProvider {
     }
 }
 
-
-//ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-//                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
-//                }
+//.toolbar {
+//            Button {
+//                showingProfile.toggle()
+//            } label: {
+//                Label("User Profile", systemImage: "person.crop.circle")
+//            }
+//        }
+//        .sheet(isPresented: $showingProfile) {
+//            ProfileHost()
+//                .environmentObject(modelData)
+//        }
